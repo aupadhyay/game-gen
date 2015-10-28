51393626ec817530d49bb1e354b1125475b0d46e
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-10-28 08:13:41 -0700

    1st update: set up of 2 containers and proved collapsing form

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+	<head>
+		<title>Game Gen</title>
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<link rel="stylesheet" href="_styles.css">
+  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	</head> 
+
+	<body>
+		<div class="main">
+			<h2>Setup</h2>
+  			<button type="button" class="theme" data-toggle="collapse" data-target="#demo">Brick Breaker</button>
+  			<div id="demo" class="collapse out">
+    			<form class="form-setup" action="test.php">
+    				<div class="test">
+    					<p>Title: </p>
+  						<input type="text" name="firstname"><br>
+  						<p>Your Name: </p>
+ 						<input type="text" name="lastname">
+ 					</div>
+    			</form>
+ 			</div>
+		</div>
+
+		<div style="width:50%;height:90%;margin-top:0px; float:right;">
+			<h2>Realtime</h2>
+  			<div class="rt-container">
+  				
+  			</div>
+		</div>
+
+	</body>
+</html>
