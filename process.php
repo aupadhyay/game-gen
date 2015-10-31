aea2f322bb9a2392ca5e0bbdcf89520748b0f08d
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-10-30 22:49:27 -0700

    Test breakthrough

--- process.php
+++ process.php
@@ -7,12 +7,17 @@ $elements = array(0 => "titleText");
 $destination = "/var/www/html/game-gen/theme1/images/";
 $uploadfile = $destination . basename($_FILES['testFile']['name']);
 
+foreach($_FILES as $file) { 
+            $n = $file['name']; 
+            $s = $file['size']; 
+        } 
+
 if (move_uploaded_file($_FILES['testFile']['tmp_name'], $uploadfile)) {
    echo "File is valid, and was successfully uploaded.\n";
 } else {
     echo $_FILES['testFile']['error'];
 }
 
-$_SESSION["test"] = $uploadfile;
+$_SESSION["test"] = $n;
 
 ?>
