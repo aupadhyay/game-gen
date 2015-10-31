0d4725d390c5a78030e40fca109bec4e3f6ff7c5
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-10-30 22:50:44 -0700

    breakthrough 2

--- process.php
+++ process.php
@@ -18,6 +18,6 @@ if (move_uploaded_file($_FILES['testFile']['tmp_name'], $uploadfile)) {
     echo $_FILES['testFile']['error'];
 }
 
-$_SESSION["test"] = $n;
+$_SESSION["test"] = $_POST['titleText'];
 
 ?>
