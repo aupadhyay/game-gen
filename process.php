3afd97f29518e007dcfcdbae9d8a5b5752d7e8de
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-10-30 23:01:44 -0700

    more debug

--- process.php
+++ process.php
@@ -8,21 +8,20 @@ $destination = "/var/www/html/game-gen/theme1/images/";
 $uploadfile = $destination . basename($_FILES['testFile']['name']);
 
 foreach($_FILES as $file) { 
-            $n = $file['name']; 
+            $n = $file['name'];
+            $k = $file['tmp_name']; 
             $s = $file['size']; 
 
             echo $n;
             $uploadfile = $destination . $n;
         } 
 
-if (move_uploaded_file($_FILES['testFile']['tmp_name'], $uploadfile)) {
+if (move_uploaded_file($k, $uploadfile)) {
    echo "File is valid, and was successfully uploaded.\n";
 } else {
     echo "error :( \n";
     echo $uploadfile;
-    echo $_FILES['testFile']['tmp_name'];
+    echo $k;
 }
-
-echo $_POST['titleText'];
 
 ?>
