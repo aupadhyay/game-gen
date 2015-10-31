bc848b5a3cdd1919b01e6c7de6652709ac83bc93
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-10-30 22:30:43 -0700

    wth did I do

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
       <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
       <script src="test.js"></script>
+      <script src="jquery.form.min.js"></script>
   </head> 
 
   <body>
@@ -13,7 +14,7 @@
       <h2>Setup</h2>
         <button type="button" class="theme" data-toggle="collapse" data-target="#demo">Brick Breaker</button>
         <div id="demo" class="collapse out">
-          <form class="form-setup" action="process.php" method="POST" enctype="multipart/form-data">
+          <form class="form-setup" id="form" action="process.php" method="POST" enctype="multipart/form-data">
             <div class="test">
               <p>Title: </p>
               <input type="text" name="titleText"><br>
@@ -27,7 +28,7 @@
 
     <div class="main">
       <h2>Realtime</h2>
-        <div class="rt-container">
+        <div class="rt-container" id="rt-container">
          	<div id="titleText"></div>
          	<div id="testImage"></div>
         </div>
--- test.js
+++ test.js
@@ -1,33 +1,20 @@
 $(document).ready(function() {
-    $('form').submit(function(event) {
+    var options = { 
+        target:        '#rt-container',
+        beforeSubmit:  showRequest,  
+        success:       showResponse  
+    }; 
 
-        var formData = {
-            'titleText': $('input[name=titleText]').val(),
-            'testImage': $('input[name=testImage]').val(),
-        };
+    $('#form').submit(function() { 
+        $(this).ajaxSubmit(options); 
 
+        return false; 
+    }); 
         
-        $.ajax({
-            type        : 'POST',
-            url         : 'process.php',
-            data        : formData,
-            dataType    : 'json',
-            encode      : true
-        })
-        
-            .done(function(recallData) {
+    function showResponse(formData, jqForm, options){
+        console.log(recallData['message']);
+        console.log(recallData['message1']);
 
-                var rtElements = [
-                    document.getElementById("titleText"),
-
-                ]
-                rtElements[0].innerHTML = recallData['titleText'];
-                console.log(recallData['message']);
-                console.log(recallData['message1']);
-
-            });
-
-        event.preventDefault();
-    });
+    }
 
 });
