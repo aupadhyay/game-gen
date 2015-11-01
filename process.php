5a35764d66b2e808eb8ccab42ce621754c5f272d
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-11-01 15:40:12 -0800

    Testing AJAX

--- process.php
+++ process.php
@@ -1,29 +1,6 @@
 <?php
-session_start();
-
-$elements = array(0 => "titleText");
-
-
-$destination = "/var/www/html/game-gen/theme1/images/";
-$uploadfile = $destination . basename($_FILES['testFile']['name']);
-
-foreach($_FILES as $file) { 
-            $n = $file['name'];
-            $k = $file['tmp_name']; 
-            $l = $file['error'];
-            $s = $file['size']; 
-
-            echo $n;
-            $uploadfile = $destination . $n;
-        } 
-
-if (move_uploaded_file($k, $uploadfile)) {
-   echo "File is valid, and was successfully uploaded.\n";
-} else {
-    echo "error :( \n";
-    echo $uploadfile;
-    echo $k;
-    echo $l;
-}
-
+	require('upload.php');
+			uploadImage($selector);
+			
+		
 ?>
