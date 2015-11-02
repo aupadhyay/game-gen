e672110a411e018aea762ec128da1a0e6193bc44
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-11-01 16:05:54 -0800

    debug

--- testing-images/ajax_php_file.php
+++ testing-images/ajax_php_file.php
@@ -19,7 +19,7 @@ echo $_FILES["file"]["name"] . " <span id='invalid'><b>already exists.</b></span
 else
 {
 $sourcePath = $_FILES['file']['tmp_name']; // Storing source path of the file in a variable
-$targetPath = "upload/".$_FILES['file']['name']; // Target path where file is to be stored
+$targetPath = "/var/www/html/game-gen/testing-images/upload/".$_FILES['file']['name']; // Target path where file is to be stored
 move_uploaded_file($sourcePath,$targetPath) ; // Moving Uploaded file
 if(move_uploaded_file($sourcePath, $target_path)) {
 	echo "<span id='success'>Image Uploaded Successfully...!!</span><br/>";
