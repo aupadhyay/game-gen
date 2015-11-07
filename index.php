da7e9f6cc2547379160a4f0007a34a6a486b9609
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-11-06 22:23:39 -0800

    Squad

--- index.php
+++ index.php
@@ -19,12 +19,13 @@ session_start();
         <button type="button" class="theme" data-toggle="collapse" data-target="#demo">Brick Breaker</button>
         <div id="demo" class="collapse out">
           <form class="form-setup" id="form" action="process.php" method="POST" enctype="multipart/form-data">
-            <div class="test">
+            <div class="test" id="test">
               <p>Title: </p>
               <input type="text" name="titleText"><br>
-              <p>Your Name: </p>
-              <input type="file" name="paddle" class="formObject" id="paddle" rt-target="testImageText">
-              <input type="file" name="testImage1" class="formObject" id="testImage1" rt-target="testImageText1">
+              <!--<p>Paddle: </p>
+              <input type="file" name="paddle" class="formObject" rt-target="paddleImage"><br>
+              <p>Ball: </p>
+              <input type="file" name="ball" class="formObject" rt-target="testImageText1">!-->
               <input type="submit">
             </div>
           </form>
@@ -34,10 +35,21 @@ session_start();
     <div class="main">
       <h2>Realtime</h2>
         <div class="rt-container" id="rt-container">
-         	<img id="testImageText" class="rt-object" src="none.png" width="88" height="14" style="margin-left: 36px; margin-top: 393px ;">
-          <img id="testImageText1" src="none.png" width="20" height="20">
+         	<img id="paddleImage" class="rt-object" src="none.png" width="88" height="14" style="margin-left: 36px; margin-top: 393px ;">
+          <img id="ballImage" src="none.png" width="20" height="20">
         </div>
     </div>
+    
+
+  <script>
+    var array = ['Paddle', 'Ball', 'Brick']
+    var div = document.getElementById('test');
+    for (var i = array.length - 1; i >= 0; i--) {
+      div.innerHTML = div.innerHTML + '<p>' + array[i] + ': </p>
+              <input type="file" name="' + array[i] + '" class="formObject" rt-target="'+ array[i]+'Image">';
+    };
+    
+  </script>
 
   </body>
 </html>
