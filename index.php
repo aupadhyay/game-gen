c6fbe64b4bc148f64429bcefad021aee6b88b146
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-11-06 20:09:19 -0800

    Remove tests

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ session_start();
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
       <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
       
-      <script src="test.js"></script>
+      <script src="realtime.js"></script>
   </head> 
 
   <body>
