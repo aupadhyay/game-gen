3fb2d13b7019ceaa69de5039ef6115b251e4c682
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-11-07 23:30:19 -0800

    Test

--- js/autoHTML.js
+++ js/autoHTML.js
@@ -9,7 +9,7 @@ var brickArray = new Array(	new Array("Title", 0),
 var div = document.getElementById('bb-form');
 for (var i = 0; i <= brickArray.length - 1; i++) {
 	if(brickArray[i][1] == 1) { //Image Ex. Paddle
-		div.innerHTML = div.innerHTML + '<p class="form-label">' + brickArray[i][0] + ': </p>  <input type="file" name="' + brickArray[i][0]+ '" class="formObject" rt-target="'+ brickArray[i][0]+'Image"> <br>';
+		div.innerHTML = div.innerHTML + '<p class="form-label">' + brickArray[i][0] + ': </p>  <input type="file" theme="brick" name="' + brickArray[i][0]+ '" class="formObject" rt-target="'+ brickArray[i][0]+'Image"> <br>';
 	}else if(brickArray[i][1] == 0) { //Text Ex.Title
 		div.innerHTML = div.innerHTML + '<p class="form-label">' + brickArray[i][0] + ': </p>  <input type="text" name="' + brickArray[i][0]+ '" class="formObject"> <br>';
 	}
@@ -27,7 +27,7 @@ var flappyArray = new Array(new Array("Title", 0),
 var div = document.getElementById('fb-form');
 for (var i = 0; i <= flappyArray.length - 1; i++) {
 	if(flappyArray[i][1] == 1) { //Image Ex. Paddle
-		div.innerHTML = div.innerHTML + '<p class="form-label">' + flappyArray[i][0] + ': </p>  <input type="file" name="' + flappyArray[i][0]+ '" class="formObject" rt-target="'+ flappyArray[i][0]+'Image"> <br>';
+		div.innerHTML = div.innerHTML + '<p class="form-label">' + flappyArray[i][0] + ': </p>  <input type="file" theme="flappy" name="' + flappyArray[i][0]+ '" class="formObject" rt-target="'+ flappyArray[i][0]+'Image"> <br>';
 	}else if(flappyArray[i][1] == 0) { //Text Ex.Title
 		div.innerHTML = div.innerHTML + '<p class="form-label">' + flappyArray[i][0] + ': </p>  <input type="text" name="' + flappyArray[i][0]+ '" class="formObject"> <br>';
 	}
