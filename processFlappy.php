1e3b0e0012cdda68a103d7f908f7848d3b829f34
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-11-08 08:41:22 -0800

    Test

--- processFlappy.php
+++ processFlappy.php
@@ -34,11 +34,18 @@
 // Zip archive will be created only after closing object
 $zip->close();
 
-header("Content-type: application/zip"); 
-header("Content-Disposition: attachment; filename=file.zip"); 
-header("Pragma: no-cache"); 
-header("Expires: 0"); 
-readfile("file.zip");
-exit;
+$file = 'file.zip';
+
+if (file_exists($file)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="'.basename($file).'"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    readfile($file);
+    exit;
+}
 		
 ?>
