3517446ee29e125b0e42a722ee5d614397e1b946
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-11-08 01:55:43 -0800

    Trying new method

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ session_start();
     <div class="main">
       <h2>Setup</h2>
         <div class="bb">
-          <button type="button" class="theme" data-toggle="collapse" data-target="#bb-collapse">Brick Breaker</button>
+          <button type="button" class="theme" data-toggle="collapse" data-target="#bb-collapse" onclick="setDivVisible('brick');">Brick Breaker</button>
           <div id="bb-collapse" class="collapse out">
             <form class="form-setup" id="form" action="process.php" method="POST" enctype="multipart/form-data">
               <div class="bb-form" id="bb-form">
@@ -31,7 +31,7 @@ session_start();
 
 
         <div class="fb">
-          <button type="button" class="theme" data-toggle="collapse" data-target="#fb-collapse">Flappy Bird</button>
+          <button type="button" class="theme" data-toggle="collapse" data-target="#fb-collapse" onclick="setDivVisible('flappy');">Flappy Bird</button>
           <div id="fb-collapse" class="collapse out">
             <form class="form-setup" id="form" action="process.php" method="POST" enctype="multipart/form-data">
               <div class="fb-form" id="fb-form">
@@ -46,15 +46,15 @@ session_start();
     <div class="main">
       <h2>Realtime</h2>
         <div class="rt-container">
-          <div class="rt-container-brick" id="rt-container">
+          <div class="rt-container-brick" name="rt-container-brick" id="rt-container" style="visibility:hidden;">
             <img id="PaddleImage" class=  "rt-object" src="none.png" width="88" height="14" style="margin-left: 36px; margin-top: 393px ;">
             <img id="BallImage" src="none.png" width="20" height="20">
           </div>
-           <div class="rt-container-flappy" id="rt-container">
+           <div class="rt-container-flappy" name="rt-container-flappy" id="rt-container" style="visibility:hidden;">
             <img id="PaddleImage" class=  "rt-object" src="none.png" width="88" height="14" style="margin-left: 36px; margin-top: 393px ;">
             <img id="BallImage" src="none.png" width="20" height="20">
           </div>
-           <div class="rt-container-cd" id="rt-container">
+           <div class="rt-container-cd" name="rt-container-cd" id="rt-container" style="visibility:hidden;">
             <img id="PaddleImage" class=  "rt-object" src="none.png" width="88" height="14" style="margin-left: 36px; margin-top: 393px ;">
             <img id="BallImage" src="none.png" width="20" height="20">
           </div>
@@ -62,6 +62,13 @@ session_start();
     </div>
     
     <script src="js/autoHTML.js"></script>
+    <script type="text/javascript">
+    $(document).ready(function() {
+      function setDivVisible (selector) {
+        $('rt-container-' + selector).attr('style', 'visibility:visible;'); 
+      }
+    }
+    </script>
 
   </body>
 </html>
