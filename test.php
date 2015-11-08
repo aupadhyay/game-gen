09517cdb389318729d8fa5f48e9fb7c7c9df2323
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-11-08 07:01:44 -0800

    Test

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 require('upload.php');
 uploadImage("testing");
 ?>
-<form action="test.php">
+<form action="test.php" method="POST">
 	<input type="file" name="testing">
 	<input type="submit">
 </form>
