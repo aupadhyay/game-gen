ae1a9b95b60c07691f4f5593fd1a4ead566e4829
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-11-08 02:05:47 -0800

    Worked

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ session_start();
     <div class="main">
       <h2>Setup</h2>
         <div class="bb">
-          <button type="button" class="theme" data-toggle="collapse" data-target="#bb-collapse" onclick="setFlappyVisible()">Brick Breaker</button>
+          <button type="button" class="theme" id="theme1" data-toggle="collapse" data-target="#bb-collapse" onclick="setBrickVisible()">Brick Breaker</button>
           <div id="bb-collapse" class="collapse out">
             <form class="form-setup" id="form" action="process.php" method="POST" enctype="multipart/form-data">
               <div class="bb-form" id="bb-form">
@@ -31,7 +31,7 @@ session_start();
 
 
         <div class="fb">
-          <button type="button" class="theme" data-toggle="collapse" data-target="#fb-collapse" onclick="setFlappyVisible()">Flappy Bird</button>
+          <button type="button" class="theme" id="theme2" data-toggle="collapse" data-target="#fb-collapse" onclick="setFlappyVisible()">Flappy Bird</button>
           <div id="fb-collapse" class="collapse out">
             <form class="form-setup" id="form" action="process.php" method="POST" enctype="multipart/form-data">
               <div class="fb-form" id="fb-form">
@@ -47,8 +47,8 @@ session_start();
       <h2>Realtime</h2>
         <div class="rt-container">
           <div class="rt-container-brick" name="rt-container-brick" id="rt-container-brick" style="visibility:hidden;">
-            <img id="PaddleImage" class=  "rt-object" src="none.png" width="88" height="14" style="margin-left: 36px; margin-top: 393px ;">
-            <img id="BallImage" src="none.png" width="20" height="20">
+            <img id="PaddleImage" class=  "rt-object" src="none.png" width="100" height="100" style="margin-left: 36px; margin-top: 393px ;">
+            <img id="BallImage" src="none.png" width="100" height="100">
           </div>
            <div class="rt-container-flappy" name="rt-container-flappy" id="rt-container-flappy" style="visibility:hidden;">
             <img id="PaddleImage" class=  "rt-object" src="none.png" width="88" height="14" style="margin-left: 36px; margin-top: 393px ;">
@@ -66,6 +66,12 @@ session_start();
     
       function setFlappyVisible () {
         $('#rt-container-flappy').attr('style', 'visibility:visible;'); 
+        $('#theme1').attr('style', 'visibility:hidden;'); 
+      }
+
+      function setBrickVisible () {
+        $('#rt-container-brick').attr('style', 'visibility:visible;'); 
+        $('#theme2').attr('style', 'visibility:hidden;'); 
       }
     </script>
 
