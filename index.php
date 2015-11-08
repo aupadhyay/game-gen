957082541c8f949d2b5f6b7599a9512147875560
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-11-08 00:26:54 -0800

    Starting Over with new method

--- index.php
+++ index.php
@@ -45,9 +45,19 @@ session_start();
 
     <div class="main">
       <h2>Realtime</h2>
-        <div class="rt-container" id="rt-container">
-          <img id="PaddleImage" class=  "rt-object" src="none.png" width="88" height="14" style="margin-left: 36px; margin-top: 393px ;">
-          <img id="BallImage" src="none.png" width="20" height="20">
+        <div class="rt-container">
+          <div class="rt-container-brick" id="rt-container">
+            <img id="PaddleImage" class=  "rt-object" src="none.png" width="88" height="14" style="margin-left: 36px; margin-top: 393px ;">
+            <img id="BallImage" src="none.png" width="20" height="20">
+          </div>
+           <div class="rt-container-flappy" id="rt-container">
+            <img id="PaddleImage" class=  "rt-object" src="none.png" width="88" height="14" style="margin-left: 36px; margin-top: 393px ;">
+            <img id="BallImage" src="none.png" width="20" height="20">
+          </div>
+           <div class="rt-container-cd" id="rt-container">
+            <img id="PaddleImage" class=  "rt-object" src="none.png" width="88" height="14" style="margin-left: 36px; margin-top: 393px ;">
+            <img id="BallImage" src="none.png" width="20" height="20">
+          </div>
         </div>
     </div>
     
