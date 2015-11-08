7e585c69d7664f56c15528145e7b9240ff3c1566
Yash-Gupta <yash0314@gmail.com>
2015-11-08 03:05:17 -0800

    Added Flappy

---
+++ flappy1/xy.lua
@@ -0,0 +1,29 @@
+tapToPlay_x = 320/2
+tapToPlay_y = 480/2
+tapToPlay_anchor_x = 0.5
+tapToPlay_anchor_y = 0.5
+
+background_x = 320/2
+background_y = 480/2
+background_anchor_x = 0.5
+background_anchor_y = 0.5
+
+player_x = 50
+player_y = 480/2
+player_anchor_x = 0.5
+player_anchor_y = 0.5	
+
+pipesTop_x = 150 
+pipesTop_y = 0
+pipesTop_anchor_x = 1
+pipesTop_anchor_y = 0
+
+pipesBottom_x = 150
+pipesBottom_y= 430
+pipesBottom_anchor_x = 1
+pipesBottom_anchor_y = 1
+
+ground_x = 0
+ground_y = 480
+ground_anchor_x = 0
+ground_anchor_y = 1
