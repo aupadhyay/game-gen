f45f003bf0d158f8525e7a5e8b4f9c0f72acb876
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-11-08 10:04:48 -0800

    edits

--- index.php
+++ index.php
@@ -76,9 +76,9 @@
             <img id="Pause-ButtonImage" class="rt-object" src="none.png" width="20" height="20" style="margin-left: 280px; margin-top: 20px;">
             <img id="Enemy1Image" class="rt-object" src="none.png" width="50" height="50" style="margin-left: 124px; margin-top: 231px;">
             <img id="Enemy2Image" class="rt-object" src="none.png" width="50" height="50" style="margin-left: 164px; margin-top: 231px;">
-            <img id="man1" class="rt-object" src="none.png" width="40" height="80" style="margin-left: 114px; margin-top: 231px;">
-            <img id="man1" class="rt-object" src="none.png" width="40" height="80" style="margin-left: 174px; margin-top: 231px;">
-            <img id="man1" class="rt-object" src="none.png" width="40" height="80" style="margin-left: 124px; margin-top: 231px;">
+            <img id="man1" class="rt-object" src="none.png" width="40" height="80" style="margin-left: 30px; margin-top: 360px;">
+            <img id="man1" class="rt-object" src="none.png" width="40" height="80" style="margin-left: 140px; margin-top: 360px;">
+            <img id="man1" class="rt-object" src="none.png" width="40" height="80" style="margin-left: 250px; margin-top: 360px;">
           </div>
         </div>
     </div>
