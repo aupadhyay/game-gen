31c871cb53f22e7ed4f2ae37bb19f294ed16818d
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-11-07 19:38:49 -0800

    switched it oops

--- js/autoHTML.js
+++ js/autoHTML.js
@@ -6,9 +6,9 @@ var array = new Array(	new Array("Title", 0),
 					  	new Array("Background", 1));
 var div = document.getElementById('bb-form');
 for (var i = 0; i <= array.length - 1; i++) {
-	if(array[i][1] == 0) { //Text Ex.Title
+	if(array[i][1] == 1) { //Image Ex. Paddle
 		div.innerHTML = div.innerHTML + '<p style="width:20px;">' + array[i][0] + ': </p>  <input type="file" name="' + array[i][0]+ '" class="formObject" rt-target="'+ array[i][0]+'Image"> <br>';
-	}else if(array[i][1] == 1) { //Image Ex. Paddle
+	}else if(array[i][1] == 0) { //Text Ex.Title
 		div.innerHTML = div.innerHTML + '<p style="width:20px;">' + array[i][0] + ': </p>  <input type="text" name="' + array[i][0]+ '" class="formObject"> <br>';
 	}
 	
