adc4353abba7bf224ed8a778810001fd6be47443
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-11-08 07:27:00 -0800

    stupid

--- process.php
+++ process.php
@@ -2,7 +2,7 @@
 	require('upload.php');
 	$items = array('Paddle', 'Ball', 'Background', 'Brick', 'PauseButton');
 	
-	for ($i=0; $i <= 1; $i++) { 
+	for ($i=0; $i <= 4; $i++) { 
 		uploadImage($items[$i]);
 	}
 		
