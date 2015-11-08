d7744b13baa5860b5482fae08fa7f621000ecd4b
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-11-08 03:06:37 -0800

    Added all games

---
+++ brick/xy.lua
@@ -0,0 +1,27 @@
+background_x = 160
+background_y = 240
+background_anchorX = 0.5
+background_anchorY = 0.5
+background_width = 320
+background_height = 480 
+
+paddle_x = 320/2
+paddle_y = 480 - 50
+paddle_anchorX = 0.5
+paddle_anchorY = 0.5
+paddle_width = 60
+paddle_height = 12
+
+ball_x = 320/2+1
+ball_y = 480/2 +175
+ball_anchorX = 0.5
+ball_anhorY = 0.5
+ball_width = 12
+ball_height = 12
+
+brick_x = _W/2
+brick_y = _H/2
+brick_anchorX = 0.5
+brick_anchorY = 0.5
+brick_width = 33
+brick_height = 18
--- flappy1/customValues.lua
+++
@@ -1,4 +0,0 @@
-scrollSpeed_int = 6
-distanceBetweenPipes_x = 300  
-firstIntervalPipeGap = 110
-secondIntervalPipeGap = 145
