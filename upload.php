4c1c33d0bc3e018011bdc1a5acbe83313a5ca087
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-11-08 06:56:34 -0800

    upload

--- upload.php
+++ upload.php
@@ -16,10 +16,6 @@ if(isset($_POST["submit"])) {
     }
 }
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png") {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     //echo "Sorry, your file was not uploaded.";
