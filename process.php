900dbe5fdf038205aef86f76e8d7ce21ab702c98
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-11-08 07:21:47 -0800

    Testing

--- process.php
+++ process.php
@@ -1,6 +1,6 @@
 <?php
 	require('upload.php');
-	$items = array('Paddle', 'Ball');
+	$items = array('Paddle', 'Ball', 'Background', 'Brick', 'PauseButton');
 	
 	for ($i=0; $i <= 1; $i++) { 
 		uploadImage($items[$i]);
