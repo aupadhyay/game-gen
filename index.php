4894a33635e8e6687268be426cf35d4bee79d19a
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2015-11-16 11:15:36 -0800

    Added default image

--- index.php
+++ index.php
@@ -58,27 +58,27 @@
       <h2>Realtime</h2>
         <div class="rt-container">
           <div class="rt-container-brick" name="rt-container-brick" id="rt-container-brick" style="visibility:hidden;">
-            <img id="BackgroundImage" class="rt-object" src="none.png" width="320" height="480" style="margin-left: 0px; margin-top: 0px;">
-            <img id="PaddleImage" class="rt-object" src="none.png" width="60" height="12" style="margin-left: 130px; margin-top: 424px;">
-            <img id="BallImage" class="rt-object" src="none.png" width="12" height="12" style="margin-left: 155px; margin-top: 409px;">
-            <img id="BrickImage" class="rt-object" src="none.png" width="33" height="18" style="margin-left: 144px; margin-top: 231px;">
-            <img id="PauseButtonImage" class="rt-object" src="none.png" width="20" height="20" style="margin-left: 280px; margin-top: 20px;">
+            <img onerror="this.src='Default.png'" id="BackgroundImage" class="rt-object" src="none.png" width="320" height="480" style="margin-left: 0px; margin-top: 0px;">
+            <img onerror="this.src='Default.png'" id="PaddleImage" class="rt-object" src="none.png" width="60" height="12" style="margin-left: 130px; margin-top: 424px;">
+            <img onerror="this.src='Default.png'" id="BallImage" class="rt-object" src="none.png" width="12" height="12" style="margin-left: 155px; margin-top: 409px;">
+            <img onerror="this.src='Default.png'" id="BrickImage" class="rt-object" src="none.png" width="33" height="18" style="margin-left: 144px; margin-top: 231px;">
+            <img onerror="this.src='Default.png'" id="PauseButtonImage" class="rt-object" src="none.png" width="20" height="20" style="margin-left: 280px; margin-top: 20px;">
           </div>
            <div class="rt-container-flappy" name="rt-container-flappy" id="rt-container-flappy" style="visibility:hidden;">
-            <img id="BackgroundFImage" class="rt-object" src="none.png" width="320" height="480" style="margin-left: 0px; margin-top: 0px;">
-            <img id="BirdImage" class="rt-object" src="none.png" width="40" height="40" style="margin-left: 30px; margin-top: 220px;">
-            <img id="TopPipesImage" class="rt-object" src="none.png" width="60" height="150" style="margin-left: 200px; margin-top: 0px;">
-            <img id="BottomPipesImage" class="rt-object" src="none.png" width="60" height="180" style="margin-left: 200px; margin-top: 250px;">
-            <img id="GroundImage" class="rt-object" src="none.png" width="320" height="50" style="margin-left: 0px; margin-top: 430px;">
+            <imgonerror="this.src='Default.png'"  id="BackgroundFImage" class="rt-object" src="none.png" width="320" height="480" style="margin-left: 0px; margin-top: 0px;">
+            <img onerror="this.src='Default.png'" id="BirdImage" class="rt-object" src="none.png" width="40" height="40" style="margin-left: 30px; margin-top: 220px;">
+            <img onerror="this.src='Default.png'" id="TopPipesImage" class="rt-object" src="none.png" width="60" height="150" style="margin-left: 200px; margin-top: 0px;">
+            <img onerror="this.src='Default.png'" id="BottomPipesImage" class="rt-object" src="none.png" width="60" height="180" style="margin-left: 200px; margin-top: 250px;">
+            <img onerror="this.src='Default.png'" id="GroundImage" class="rt-object" src="none.png" width="320" height="50" style="margin-left: 0px; margin-top: 430px;">
           </div>
            <div class="rt-container-cd" name="rt-container-cd" id="rt-container-cd" style="visibility:hidden;">
-            <img id="BGImage" class="rt-object" src="none.png" width="320" height="480" style="margin-left: 0px; margin-top: 0px;">
-            <img id="Pause-ButtonImage" class="rt-object" src="none.png" width="20" height="20" style="margin-left: 280px; margin-top: 20px;">
-            <img id="Enemy1Image" class="rt-object" src="none.png" width="50" height="50" style="margin-left: 35px; margin-top: 60px;">
-            <img id="Enemy2Image" class="rt-object" src="none.png" width="50" height="50" style="margin-left: 235px; margin-top: 60px;">
-            <img id="Man1Image" class="rt-object" src="none.png" width="40" height="80" style="margin-left: 30px; margin-top: 360px;">
-            <img id="Man2Image" class="rt-object" src="none.png" width="40" height="80" style="margin-left: 140px; margin-top: 360px;">
-            <img id="Man3Image" class="rt-object" src="none.png" width="40" height="80" style="margin-left: 250px; margin-top: 360px;">
+            <img onerror="this.src='Default.png'" id="BGImage" class="rt-object" src="none.png" width="320" height="480" style="margin-left: 0px; margin-top: 0px;">
+            <img onerror="this.src='Default.png'" id="Pause-ButtonImage" class="rt-object" src="none.png" width="20" height="20" style="margin-left: 280px; margin-top: 20px;">
+            <img onerror="this.src='Default.png'" id="Enemy1Image" class="rt-object" src="none.png" width="50" height="50" style="margin-left: 35px; margin-top: 60px;">
+            <img onerror="this.src='Default.png'" id="Enemy2Image" class="rt-object" src="none.png" width="50" height="50" style="margin-left: 235px; margin-top: 60px;">
+            <img onerror="this.src='Default.png'" id="Man1Image" class="rt-object" src="none.png" width="40" height="80" style="margin-left: 30px; margin-top: 360px;">
+            <img onerror="this.src='Default.png'" id="Man2Image" class="rt-object" src="none.png" width="40" height="80" style="margin-left: 140px; margin-top: 360px;">
+            <img onerror="this.src='Default.png'" id="Man3Image" class="rt-object" src="none.png" width="40" height="80" style="margin-left: 250px; margin-top: 360px;">
           </div>
         </div>
     </div>
